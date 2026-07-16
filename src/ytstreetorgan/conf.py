1013aac7ffdc32c20872de3c0c029c55e6e888bd
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2026-07-16 12:54:53 +0900

    UPD: refactoring

--- src/ytstreetorgan/conf.py
+++ src/ytstreetorgan/conf.py
@@ -21,87 +21,62 @@ class Conf:
         """Constructor."""
         logger.debug(f'config_file=\'{config_file}\'')
 
-        self.config_file: Path | None = None
+        self.config_file = Path(config_file).expanduser()
+
         self.data: list[dict] = []
         self.models: list[str] = []
 
-        conf_path: Path = Path(config_file).expanduser()
-        logger.debug(f'conf_path=\'{conf_path}\'')
+        #
+        # `config_file`が指定されなければ、SEARCH_PATHを探す
+        #
+        if config_file == '':
+            for dir in self.SEARCH_PATH:
+                self.config_file = (dir / self.CONF_FNAME).expanduser()
+                logger.debug(f'search config_file=\'{self.config_file}\'')
 
-        if conf_path.is_file():
-            self.config_file = conf_path
-        else:
-            self.config_file = self.search()
+                if self.config_file.is_file():
+                    logger.debug(f'find: \'{self.config_file}\'')
+                    break
 
-        if self.config_file is None:
-            logger.error('config_file is None')
-        else:
+        if self.config_file.is_file():
             self.load()
+        else:
+            logger.error(f'{self.config_file.name}: not found')
+            raise FileNotFoundError(self.config_file.name)
 
-    def search(self) -> Path | None:
-        """Search config file."""
-        logger.debug('')
-
-        for dir in self.SEARCH_PATH:
-            conf_path: Path = (dir / self.CONF_FNAME).expanduser()
-            logger.debug(f'conf_path=\'{conf_path}\'')
-            
-            if conf_path.is_file():  # file is found
-                logger.debug(f'find: \'{conf_path}\'')
-                self.config_file = conf_path
-                return self.config_file
-
-        logger.error(f'\'{self.CONF_FNAME}\': not found')
-        return None
-        
     def load(self) -> list:
         """Load config file."""
-        logger.debug(f'config_file={self.config_file}')
-
-        if self.config_file is None:
-            logger.error('config_file is None')
-            return []
+        logger.debug(f'config_file=\'{self.config_file}\'')
 
         try:
             json_text = self.config_file.read_text(encoding='utf-8')
             self.data = json.loads(json_text)
+            self.models = [d['model'] for d in self.data]
+        except UnicodeDecodeError as e:
+            logger.error(exmsg(e))
+            return []
         except json.JSONDecodeError as e:
+            logger.error(exmsg(e))
+            return []
+        except KeyError as e:
             logger.error(exmsg(e))
             return []
         except Exception as e:
             logger.error(exmsg(e))
             return []
             
-        self.models = self.get_models()
         return self.data
 
-    def get(self, model_name=''):
+    def get(self, model_name='') -> dict:
         """Get config data for ``model_name``."""
         logger.debug(f'model_name=\'{model_name}\'')
 
         if self.data is None:
-            return None
+            return {}
 
         for d in self.data:
-            if d['model'] == model_name:
+            if d.get('model') == model_name:
                 return d
 
         logger.error(f'mode:\'{model_name}\' not found')
-        return None
-
-    def get_models(self) -> list[str]:
-        """Get model_name list."""
-        logger.debug('')
-
-        self.models = []
-
-        if self.data is None:
-            logger.error('data is None')
-        else:
-            try:
-                self.models = [d['model'] for d in self.data]
-            except KeyError as e:
-                logger.error(exmsg(e))
-            except Exception as e:
-                logger.error(exmsg(e))
-        return self.models
+        return {}
